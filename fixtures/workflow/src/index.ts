6f81b7ef387abe6bcda6140e37a8ed2d2ada4064
Luís Duarte <lduarte@cloudflare.com>
2025-03-20 18:27:03 +0000

    feat: implement local dev for createBatch

--- fixtures/workflow/src/index.ts
+++ fixtures/workflow/src/index.ts
@@ -27,7 +27,7 @@ export class Demo extends WorkflowEntrypoint<{}, Params> {
 			};
 		});
 
-		return "i'm a workflow output";
+		return payload ?? "no-payload";
 	}
 }
 
@@ -44,13 +44,21 @@ export default class extends WorkerEntrypoint<Env> {
 		}
 
 		let handle: WorkflowInstance;
-		if (url.pathname === "/create") {
+		if (url.pathname === "/createBatch") {
+			// creates two instances
+			const batch = await this.env.WORKFLOW.createBatch([
+				{ id: "batch-1", params: "1" },
+				{ id: "batch-2", params: "2" },
+			]);
+			return Response.json(batch.map((instance) => instance.id));
+		} else if (url.pathname === "/create") {
 			if (id === null) {
 				handle = await this.env.WORKFLOW.create();
 			} else {
 				handle = await this.env.WORKFLOW.create({ id });
 			}
 		} else {
+			console.log("id", id);
 			handle = await this.env.WORKFLOW.get(id);
 		}
 
